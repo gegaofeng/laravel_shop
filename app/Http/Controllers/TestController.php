a5cb8a3d9c3e217d470f789137f6de2bf9ab2542
gegaofeng <geahvshd@gmail.com>
2018-12-18 22:41:53 +0800

    login
    Signed-off-by: gegaofeng <geahvshd@gmail.com>

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -45,43 +45,5 @@ class TestController extends Controller
 //        $request->session()->regenerate();
         var_dump($request->session()->all());
         var_dump($request->session()->get('_token'));
-//        $a=GoodsSpec::with('goodsSpecItem')->whereIn('id',[1,2])->get();
-//        $a=GoodsSpecItem::with('spec')->whereIn('id',[9,11,16,12,13])->get();
-//        return $a;
-//        $data=array(
-//            array('name'=>'namea','b','goods_spec_item'=>array(array('id'=>'aa','bb','cc'),array('id'=>'aa','bb','cc'))),
-//          array('name'=>'nameaa','bb','cc','goods_spec_item'=>array(array('id'=>'aa','bb','cc'),array('id'=>'aa','bb','cc'))),
-//        );
-//        $data=json_encode($data);
-////        return $data;
-//        return view('test')->with('data',$data);
-//        return $this->model->getGoodsById(96);
-//        $tools=new Tools();
-//        return $tools::create_goods_thum_images(202,236,236);
-//        $goodsSpecPriceRepository=new GoodsSpecPriceRepository();
-//         $spec_goods_price=$goodsSpecPriceRepository->getGoodsSpecPriceByGoodsId(1);
-//        return view('test')->with('spec_goods_price',$spec_goods_price);
-//        $a=new CartRepository();
-//        return $a->setGoodsRepository(1211);
-//        return $a->getCartList('0');
-//        $tools=new Tools();
-//        return $tools::create_goods_thum_images(88,236,236);
-//        $goods_cat=new GoodsCategoryRepository();
-//        return view('test')->with('test',$goods_cat-> getSortGoodsCategory());
-//        return $goods_cat-> getCatSonTree(14);
-//        return 1;
-//        $brand=new \App\Repositories\BrandRepository();
-//        return $brand-> getSortBrands(33);
-//        $cat=new GoodsCategoryRepository();
-//        return $cat-> getCatSonTree(13);
-
-//        $a=new GoodsRepository();
-//        return view('test')->with('a',$a-> getGoodsById(1));
-//        var_dump($a-> getGoodsById(1));
-//        $a=new \App\Repositories\GoodsSpecRepository();
-//        var_dump($a-> getSpecListByTypeId(1));
-//         return $a-> getSpecListByTypeId(1);
-//        $a=new GoodsSpecPriceRepository();
-//        return $a-> getGoodsSpecItemIdByGoodsId(104);
     }
 }
